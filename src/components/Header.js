ddec28a228dc1c3e93a75b85d8f58a4a24d49516
sajibur-rahman <sajiburr21@gmail.com>
2022-04-04 12:02:04 +0600

    active link style is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,60 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from '../Module/Header.module.css';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../Module/Header.module.css";
 
 const Header = () => {
-    return (
-        <div className={styles.nav}>
-            <NavLink className={styles.navlink}  to='/home'>Home</NavLink>
-            <NavLink className={styles.navlink} to="/review">review</NavLink>
-            <NavLink className={styles.navlink} to="/dashboard">dashboard</NavLink>
-            <NavLink className={styles.navlink} to="/blogs">blogs</NavLink>
-            <NavLink className={styles.navlink} to="/about">about</NavLink>
-        </div>
-    );
+  const activeClass = {
+    color: "red",
+  };
+  return (
+    <div className={styles.nav}>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: "red" } : { color: "black" }
+        }
+        className={styles.navlink}
+        to="/home"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: "red" } : { color: "black" }
+        }
+        className={styles.navlink}
+        to="/review"
+      >
+        review
+      </NavLink>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: "red" } : { color: "black" }
+        }
+        className={styles.navlink}
+        to="/dashboard"
+      >
+        dashboard
+      </NavLink>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: "red" } : { color: "black" }
+        }
+        className={styles.navlink}
+        to="/blogs"
+      >
+        blogs
+      </NavLink>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: "red" } : { color: "black" }
+        }
+        className={styles.navlink}
+        to="/about"
+      >
+        about
+      </NavLink>
+    </div>
+  );
 };
 
 export default Header;
