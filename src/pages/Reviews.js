76c937f4bc34a22ef0dd95f4271e1349bdb85ed4
sajibur-rahman <sajiburr21@gmail.com>
2022-04-04 10:12:10 +0600

    review page is done

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Review from '../components/Review';
+import useFatchReview from '../hooks/useFatchReview';
+import Styles from '../Module/Reviews.module.css';
 
-const Review = () => {
+
+const Reviews = () => {
+    const [reviews] =  useFatchReview()
     return (
-        <div>
-            review
+        <div className={Styles.review_container}>
+            <h1 className={Styles.review_title}>Every review here...</h1>
+            <div className={Styles.user_reviews}>
+                {
+                    reviews ? reviews.map((review) => <Review key={review.id} review={review}/>) : null
+                }
+            </div>
         </div>
     );
 };
 
-export default Review;
+export default Reviews;
